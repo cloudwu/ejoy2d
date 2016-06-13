50de0a82acf4068b4381e7d984ebce47a074892f
cabrite <cabriter@vip.qq.com>
2016-06-13 23:46:10 +0800

    test export import simplepack,understand win->ejoy2d binding code

---
+++ examples/asset/box.lua
@@ -0,0 +1,26 @@
+return {
+
+{
+	type = "label",
+	id = 1,
+	export = "default_label",
+	width = 200,
+	height = 100,
+	font = "",
+	align = 0,
+	size = 16,
+	color = 0xffffffff,
+	noedge = true,
+},
+{
+	type = "picture",
+	id = 0,
+	export = "box1",
+	{
+		tex = 1,
+		src = { 1, 1, 1, 65, 65, 65, 65, 1 },
+		screen = { -512, -512, -512, 512, 512, 512, 512, -512 },
+	},
+},
+
+}
